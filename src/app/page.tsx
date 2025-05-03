4b176555786b199eb351108ad57c00e038913aee
gurnit <gurjiitsingh@gmail.com>
2025-05-03 15:51:28 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,14 @@ import { FaBars } from "react-icons/fa";
 import { UseSiteContext } from "@/SiteContext/SiteContext";
 import Header from "@/components/Header";
 import ServicesSection from "@/components/Services";
-import CreateWebsite from "@/components/Create-your-website";
-import FeaturesSection from "@/components/FeaturesSection";
-import WhyChooseUs from "@/components/Why-choose-us";
-import Slider from "@/components/Slider";
+
 import HeroSection from "@/components/Hero";
 import AboutUsSection from "@/components/about-us";
 import WhyChooseUsSection from "@/components/Why-choose-us";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import SliderSection from "@/components/Slider";
 import Footer from "@/components/Footer";
-import Footer1 from "@/components/Footer-1";
+
 //import { usePathname } from "next/navigation";
 // import { Carousel } from "react-responsive-carousel";
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -40,7 +37,7 @@ export default function Home() {
 
 
   return (
-    <div className="w-full mx-auto bg-gray-100 text-gray-900 py-7">
+    <div className="w-full mx-auto bg-gray-100 text-gray-900 ">
       {/* Header */}
       <Header />
       <HeroSection />
